704f69eccdcbbbaa78f6941d99f4a2bf17f2e29d
Aditya sharma <aadijaipur19@gmail.com>
2022-03-19 10:58:08 +0530

    create outlet of ui label

--- udder/ViewController.swift
+++ udder/ViewController.swift
@@ -10,11 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tagLineLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+  
 
 }
 
